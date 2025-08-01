eb64d66fe4f71c983572e93b2428135d1afbb24e
yoavbenyaakov <yoavb1@mail.tu.ac.il>
2025-08-01 23:02:09 +0300

    Initial commit without secrets

---
+++ personal_web/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),  # for http://127.0.0.1:8000/
+    path('projects/', views.projects, name='projects'),
+    path('publications/', views.publications, name='publications'),
+    path('teaching/', views.teaching, name='teaching'),
+    path('contact/', views.contact, name='contact'),
+]
---
+++ personal_web/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home.html')
+def projects(request):
+    return render(request, 'projects.html')
+
+def publications(request):
+    return render(request, 'publications.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def teaching(request):
+    return render(request, 'teaching.html')
+
+
